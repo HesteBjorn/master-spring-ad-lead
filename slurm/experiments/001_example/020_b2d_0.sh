f351d6883c0ef376cb46b6d4417d7bfdc3b97305
Erik Hæstad Bjørnstad <erikhosle@gmail.com>
2026-01-25 19:15:44 +0100

    add: full run spec b2d script

--- slurm/experiments/001_example/020_b2d_0.sh
+++ slurm/experiments/001_example/020_b2d_0.sh
@@ -3,7 +3,7 @@
 export CARLA_ROOT=3rd_party/CARLA_0915
 export PYTHONPATH="$CARLA_ROOT/PythonAPI:$CARLA_ROOT/PythonAPI/carla:$PWD:$PYTHONPATH"
 # SLURM generator parameters for per-route jobs
-export EVALUATION_PARAMETERS="--id_list 10857"  # COMMENT OUT THIS FOR FULL RUN
+# export EVALUATION_PARAMETERS="--id_list 10857"  # COMMENT OUT THIS FOR FULL RUN
 export SCRIPT_GENERATOR_PARAMETERS="--partition=GPUQ --slurm_timeout 0-01:00:00 --repetitions 1"
 export WANDB_MODE=disabled
 
